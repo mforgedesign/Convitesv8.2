747e6aca63ebd53cee76a051a92403bb2792fa2d
Builder v8 <builder@mforge.com.br>
2026-07-29 17:16:43 -0300

    Deploy convite: Amanda&Cesar-BodasDePrata

---
+++ Amanda&Cesar-BodasDePrata/config.js
@@ -0,0 +1,187 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Amanda e César",
+    "idade": 0,
+    "data": "2026-08-22",
+    "hora": "18:30",
+    "horaTermino": "",
+    "endereco": "",
+    "frase1": "",
+    "frase2": "",
+    "frase3": ""
+  },
+  "convite": {
+    "paletaCores": "Dourado e Branco",
+    "tema": "Elegante",
+    "musica": "assets/music_1785356202599.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": false,
+    "slug": "Amanda&Cesar-BodasDePrata",
+    "seo": {
+      "pageTitle": "Amanda e César | Bodas de Prata",
+      "ogTitle": "Amanda e César | Bodas de Prata",
+      "ogDescription": "Clique para ver o convite interativo!"
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1785356202599.png",
+    "cartao": "",
+    "aberturaSlides": [
+      "assets/slide1_1785356202599.mp4",
+      "assets/slide2_1785356202599.mp4",
+      "assets/slide3_1785356202599.mp4",
+      "assets/slide4_1785356202599.mp4"
+    ],
+    "aberturaSlidesMeta": [
+      {
+        "filePath": "assets/slide1_1785356202599.mp4",
+        "label": "Slide 1",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide2_1785356202599.mp4",
+        "label": "Slide 1",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide3_1785356202599.mp4",
+        "label": "Slide 2",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide4_1785356202599.mp4",
+        "label": "Slide 3",
+        "type": "video"
+      }
+    ],
+    "folhaVazia": "assets/folha_vazia_1785356202599.png",
+    "folhaPreenchida": "assets/folha_1785356202599.mp4",
+    "musica": "assets/music_1785356202599.mp3",
+    "musicStartSec": 0,
+    "musicEndSec": null,
+    "fabric": {
+      "saveTheDate": {
+        "referencia": "",
+        "resultado": "",
+        "musicaPersonalizada": "",
+        "musicSource": "convite",
+        "hasMusic": true,
+        "musicStartSec": 0,
+        "durationSec": 20
+      },
+      "lembrete": {
+        "referencia": "",
+        "resultado": "",
+        "musicaPersonalizada": "",
+        "musicSource": "convite",
+        "hasMusic": true,
+        "musicStartSec": 0,
+        "durationSec": 20
+      }
+    },
+    "fotos": [
+      "assets/foto1_1785356202599.jpg",
+      "assets/foto2_1785356202599.jpg",
+      "assets/foto3_1785356202599.jpg",
+      "assets/foto4_1785356202599.jpg",
+      "assets/foto5_1785356202599.jpg",
+      "assets/foto6_1785356202599.jpg",
+      "assets/foto7_1785356202599.jpg",
+      "assets/foto8_1785356202599.jpg",
+      "assets/foto9_1785356202599.jpg",
+      "assets/foto10_1785356202599.jpg",
+      "assets/foto11_1785356202599.jpeg",
+      "assets/foto12_1785356202599.jpeg",
+      "assets/foto13_1785356202599.jpeg",
+      "assets/foto14_1785356202599.jpeg",
+      "assets/foto15_1785356202599.jpeg",
+      "assets/foto16_1785356202599.jpeg",
+      "assets/foto17_1785356202599.jpeg"
+    ],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "id": "btn_legacy_location",
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/F6H4cw8nACVrVPM8A"
+    },
+    {
+      "id": "btn_legacy_manual",
+      "tipoAcao": "PopupImagem",
+      "tipoVisual": "css",
+      "titulo": "Manual",
+      "icone": "fa-solid fa-book-open",
+      "conteudo": "assets/manual_1775865108445_1785356202599.png"
+    },
+    {
+      "id": "btn_legacy_rsvp",
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-brands fa-whatsapp",
+      "whatsapp": "558588582020",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": true,
+        "exibirAcompanhantes": false
+      }
+    },
+    {
+      "id": "btn_legacy_gifts",
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Presentes",
+      "icone": "fa-solid fa-gift",
+      "conteudo": "https://presentes.mforge.com.br/amanda-e-cesar"
+    },
+    {
+      "id": "btn_legacy_gallery_link",
+      "tipoAcao": "Galeria",
+      "tipoVisual": "css",
+      "titulo": "Galeria de Fotos",
+      "icone": "fa-solid fa-camera",
+      "conteudo": [
+        "assets/foto1_1785356202599.jpg",
+        "assets/foto2_1785356202599.jpg",
+        "assets/foto3_1785356202599.jpg",
+        "assets/foto4_1785356202599.jpg",
+        "assets/foto5_1785356202599.jpg",
+        "assets/foto6_1785356202599.jpg",
+        "assets/foto7_1785356202599.jpg",
+        "assets/foto8_1785356202599.jpg",
+        "assets/foto9_1785356202599.jpg",
+        "assets/foto10_1785356202599.jpg",
+        "assets/foto11_1785356202599.jpeg",
+        "assets/foto12_1785356202599.jpeg",
+        "assets/foto13_1785356202599.jpeg",
+        "assets/foto14_1785356202599.jpeg",
+        "assets/foto15_1785356202599.jpeg",
+        "assets/foto16_1785356202599.jpeg",
+        "assets/foto17_1785356202599.jpeg"
+      ]
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": true,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": true,
+    "buttonColor": "#6a7b74",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": false,
+      "direito": false,
+      "centro": false
+    }
+  }
+};
