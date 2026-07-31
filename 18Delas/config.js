f25aaf929624c677978084cf5ac5c49cbb1fef51
Builder v8 <builder@mforge.com.br>
2026-07-31 12:35:52 -0300

    Deploy convite: 18Delas

--- 18Delas/config.js
+++ 18Delas/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Dourado e Champagne",
     "tema": "Minimalista Elegante, Perfume Fragâncias",
-    "musica": "assets/music_1785511899046.mp3",
+    "musica": "assets/music_1785512152414.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "18Delas",
@@ -25,33 +25,33 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1785511899046.jpg",
+    "capa": "assets/cover_1785512152414.jpg",
     "cartao": "",
     "aberturaSlides": [
-      "assets/slide1_1785511899046.mp4",
-      "assets/slide2_1785511899046.mp4",
-      "assets/slide3_1785511899046.mp4"
+      "assets/slide1_1785512152414.mp4",
+      "assets/slide2_1785512152414.mp4",
+      "assets/slide3_1785512152414.mp4"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1785511899046.mp4",
+        "filePath": "assets/slide1_1785512152414.mp4",
         "label": "Abertura (Vídeo)",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1785511899046.mp4",
+        "filePath": "assets/slide2_1785512152414.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide3_1785511899046.mp4",
+        "filePath": "assets/slide3_1785512152414.mp4",
         "label": "Slide 1",
         "type": "video"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1785511899046.png",
-    "folhaPreenchida": "assets/folha_1785511899046.jpg",
-    "musica": "assets/music_1785511899046.mp3",
+    "folhaVazia": "assets/folha_vazia_1785512152414.png",
+    "folhaPreenchida": "assets/folha_1785512152414.jpg",
+    "musica": "assets/music_1785512152414.mp3",
     "musicStartSec": 0,
     "musicEndSec": null,
     "fabric": {
