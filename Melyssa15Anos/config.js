78e64746d4424a9f9af31fc535b1585c753926cb
Builder v8 <builder@mforge.com.br>
2026-08-05 15:59:35 -0300

    Deploy convite: Melyssa15Anos

--- Melyssa15Anos/config.js
+++ Melyssa15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Rosa/Rosé",
     "tema": "Floral",
-    "musica": "assets/music_1785951493076.mp3",
+    "musica": "assets/music_1785956375061.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Melyssa15Anos",
@@ -25,39 +25,39 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1785951493076.png",
-    "cartao": "assets/cartao_chatgpt_1785951493076.jpg",
+    "capa": "assets/cover_1785956375061.png",
+    "cartao": "assets/cartao_chatgpt_1785956375061.jpg",
     "aberturaSlides": [
-      "assets/slide1_1785951493076.mp4",
-      "assets/slide2_1785951493076.jpg",
-      "assets/slide3_1785951493076.mp4",
-      "assets/slide4_1785951493076.mp4"
+      "assets/slide1_1785956375061.mp4",
+      "assets/slide2_1785956375061.jpg",
+      "assets/slide3_1785956375061.mp4",
+      "assets/slide4_1785956375061.mp4"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1785951493076.mp4",
+        "filePath": "assets/slide1_1785956375061.mp4",
         "label": "Abertura (Video)",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1785951493076.jpg",
+        "filePath": "assets/slide2_1785956375061.jpg",
         "label": "Convite Especial",
         "type": "image"
       },
       {
-        "filePath": "assets/slide3_1785951493076.mp4",
+        "filePath": "assets/slide3_1785956375061.mp4",
         "label": "Slide 2",
         "type": "video"
       },
       {
-        "filePath": "assets/slide4_1785951493076.mp4",
-        "label": "Cena Tematica",
+        "filePath": "assets/slide4_1785956375061.mp4",
+        "label": "Slide 1",
         "type": "video"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1785951493076.jpg",
-    "folhaPreenchida": "assets/folha_1785951493076.png",
-    "musica": "assets/music_1785951493076.mp3",
+    "folhaVazia": "assets/folha_vazia_1785956375061.jpg",
+    "folhaPreenchida": "assets/folha_1785956375061.png",
+    "musica": "assets/music_1785956375061.mp3",
     "musicStartSec": 0,
     "musicEndSec": null,
     "fabric": {
