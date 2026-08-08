f17ad0d5b171b8df7d338976f6ce243d3fc5fcfe
Builder v8 <builder@mforge.com.br>
2026-08-08 15:57:56 -0300

    Deploy convite: BeatrizGouveia-15Anos

---
+++ BeatrizGouveia-15Anos/config.js
@@ -0,0 +1,124 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Beatriz Gouveia ",
+    "idade": 15,
+    "data": "2026-11-19",
+    "hora": "21:00",
+    "horaTermino": "",
+    "endereco": "Local: Imperial Pallace Buffet\nRua S, nº540 - Maraponga | Mondubim\nFortaleza/CE\n",
+    "frase1": "",
+    "frase2": "",
+    "frase3": ""
+  },
+  "convite": {
+    "paletaCores": "Preto, Vinho, Azul-petróleo, Dourado",
+    "tema": "Alice no País das Maravilhas",
+    "musica": "assets/music_1786215474403.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "BeatrizGouveia-15Anos",
+    "seo": {
+      "pageTitle": "Convite Especial",
+      "ogTitle": "Você recebeu um convite especial!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1786215474403.png",
+    "cartao": "assets/cartao_chatgpt_1786215474403.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1786215474403.mp4",
+      "assets/slide2_1786215474403.mp4",
+      "assets/slide3_1786215474403.jpg",
+      "assets/slide4_1786215474403.mp4"
+    ],
+    "aberturaSlidesMeta": [
+      {
+        "filePath": "assets/slide1_1786215474403.mp4",
+        "label": "Abertura (Vídeo)",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide2_1786215474403.mp4",
+        "label": "Slide 1",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide3_1786215474403.jpg",
+        "label": "Slide 2",
+        "type": "video"
+      },
+      {
+        "filePath": "assets/slide4_1786215474403.mp4",
+        "label": "Slide 3",
+        "type": "video"
+      }
+    ],
+    "folhaVazia": "assets/folha_vazia_1786215474403.png",
+    "folhaPreenchida": "assets/folha_1786215474403.jpg",
+    "musica": "assets/music_1786215474403.mp3",
+    "musicStartSec": 0,
+    "musicEndSec": null,
+    "fabric": {
+      "saveTheDate": {
+        "referencia": "",
+        "resultado": "",
+        "musicaPersonalizada": "",
+        "musicSource": "convite",
+        "hasMusic": true,
+        "musicStartSec": 0,
+        "durationSec": 20
+      },
+      "lembrete": {
+        "referencia": "",
+        "resultado": "",
+        "musicaPersonalizada": "",
+        "musicSource": "convite",
+        "hasMusic": true,
+        "musicStartSec": 0,
+        "durationSec": 20
+      }
+    },
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "id": "btn_c967487f-140a-48f7-a1cb-a1aa61bb2c54",
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/ePxJRkiNSo2CcoiX9"
+    },
+    {
+      "id": "btn_192ac197-6217-4fbe-9f6f-3e52ec478349",
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "conteudo": "558587836809"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#0c2b71",
+    "buttonGradientColor": "#ec4899",
+    "buttonGradientEnabled": false,
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
